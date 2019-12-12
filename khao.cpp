5c83ed016b1bd72ca236209555fbd826b6d58e40
kongqi <32472226+kongqiahaha@users.noreply.github.com>
2019-12-12 17:13:42 +0800

    k好数

--- khao.cpp
+++ khao.cpp
@@ -2,8 +2,8 @@
 #include<math.h>
 int main()
 {
-	int l, k, a[100][100] = {0};
-	long long int s = 0;
+	int l, k;
+	long long int s = 0, a[100][100] = { 0 };
 	scanf_s("%d %d", &k, &l);
 	for (int i = 0; i < l; i++)
 	{
@@ -17,10 +17,11 @@ int main()
 				for (int m = 0; m < k; m++)
 					if (abs(j - m) != 1)
 						a[i][j] = a[i][j] + a[i - 1][m];
+			a[i][j] = a[i][j]%1000000007;
 		}
 	}
 	for (int i = 0; i < k; i++)
-		s = s + a[l-1][i];
-	printf("%d", s%1000000007);
+		s = s + a[l - 1][i];
+	printf("%lld", s=s % 1000000007);
 	return 0;
 }
