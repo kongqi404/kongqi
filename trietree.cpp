18e44ddafb180fd3e8a854f71a82aeee2550d783
kongqi <32472226+kongqiahaha@users.noreply.github.com>
2020-03-06 22:40:34 +0800

    字典树

---
+++ trietree.cpp
@@ -0,0 +1,56 @@
+#include<stdio.h>
+#include<malloc.h>
+#include<string.h>
+#define MAXSIZE 26
+typedef struct trietree
+{
+	int flag;
+	trietree* next[MAXSIZE];
+}trie;
+trie* creatnode()
+{
+	trie* p = (trie*)malloc(sizeof(trie));
+	p->flag = 1;
+	for (int i = 0; i < MAXSIZE; i++)
+		p->next[i] = NULL;
+	return p;
+}
+void insertnode(trie *root,char* s,int i)
+{
+	if (i == strlen(s))
+		return;
+	if (root->next[s[i] - 'a'] == NULL)
+	{
+		trie* p = creatnode();
+		root->next[s[i] - 'a'] = p;
+		insertnode(p, s, i + 1);
+	}
+	else
+	{
+		root->flag++;
+		insertnode(root->next[s[i] - 'a'], s, i + 1);
+	}
+}
+int search(trie* root, char* s, int i)
+{
+	if(i==strlen(s)&&root->flag==1)
+		return 1;
+	if (root->next[s[i] - 'a'] != NULL)
+		return search(root->next[s[i] - 'a'], s, i + 1);
+	else
+		return 0;
+}
+int main()
+{
+	trie* root = creatnode();
+	int n;
+	char s[100];
+	scanf_s("%d", &n);
+	for (int i = 0; i < n; i++)
+	{
+		scanf_s("%s", &s, 100);
+		insertnode(root, s, 0);
+	}
+	scanf_s("%s", &s, 100);
+	printf("%d", search(root, s, 0));
+}
